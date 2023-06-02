dbbe3ba20f71492ba85129c3002840d26296380a
Huy Cao <anhhuy.cao@gmail.com>
2023-06-02 14:45:52 +0700

    Update config keepalived mode active active

--- keepalived-haproxy/start.sh
+++ keepalived-haproxy/start.sh
@@ -4,6 +4,6 @@ echo "***** Start HAProxy *****"
 /usr/sbin/haproxy -f /usr/local/etc/haproxy/haproxy.cfg &
 sleep 1
 echo "***** Start Keepalived *****"
-/usr/local/sbin/keepalived -f /usr/local/etc/keepalived/keepalived.conf --dont-fork --log-console &
+/usr/local/sbin/keepalived -f /usr/local/etc/keepalived/keepalived.conf --dont-fork --log-console --log-detail &
 wait -n
 exit $?
