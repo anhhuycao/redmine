e2c87528ded1e559069b88ff1036ec698f4c197e
Huy Cao <anhhuy.cao@gmail.com>
2023-08-02 20:16:19 +0700

    Fix permission jenkins

--- devops/jenkins/install.sh
+++ devops/jenkins/install.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+mkdir /var/jenkins_home
+chown 1000 /var/jenkins_home
 docker run -d --name jenkins -p 8081:8080 -p 50000:50000 -v /var/jenkins_home:/var/jenkins_home jenkins/jenkins:lts-jdk11
