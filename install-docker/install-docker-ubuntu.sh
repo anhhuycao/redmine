3fe0d28d973a02c7967d352df6f24c722d0d2141
Huy Cao <huyca@dts.com.vn>
2023-12-25 10:32:42 +0700

    Install docker

---
+++ install-docker/install-docker-ubuntu.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt update \
+&& sudo apt -y install apt-transport-https ca-certificates curl gnupg2 software-properties-common \
+&& curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg \
+&& echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list \
+&& sudo apt update \
+&& sudo apt install -y docker-ce \
+&& sudo usermod -aG docker ${USER}
